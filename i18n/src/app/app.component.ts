15c46db4525c4e573cb06837c31eb189ae1f7aff
jluzio <joao.luzio@gmail.com>
2017-06-06 19:21:40 +0100

    i18n example

--- i18n/src/app/app.component.ts
+++ i18n/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "i18n examples";
+  wolves = 0;
+  gender = 'f';
+  fly = true;
+  logo = 'https://angular.io/resources/images/logos/angular/angular.png';
+  inc(i: number) {
+    this.wolves = Math.min(5, Math.max(0, this.wolves + i));
+  }
+  male()   { this.gender = 'm'; }
+  female() { this.gender = 'f'; }
 }
